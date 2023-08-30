0860344854f79215367757643e46282d3bb23763
ZoeMesaros <zoe.mesaros@hotmail.com>
2023-08-30 12:36:08 +0200

    gif

--- public/room.js
+++ public/room.js
@@ -9,12 +9,45 @@ const leaveBtn = document.getElementById("leaveBtn");
 const chattInput = document.getElementById("chattInput");
 const chattBtn = document.getElementById("chattBtn");
 const chatt = document.getElementById("chatt");
-const userList = document.getElementById("userList"); 
+const userList = document.getElementById("userList");
 const typingIndicator = document.getElementById("typingIndicator");
+const searchInput = document.getElementById("gif-search");
+const searchBtn = document.getElementById("gif-search-btn");
+const gifDiv = document.getElementById("gifDiv");
+
+const apiKey = "Ve81IpKHTwK7F5llof3SYaTP6fVhbARU";
+
+const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}`;
+
+function searchGifs(query) {
+  return fetch(`${apiUrl}&q=${query}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const gifUrls = data.data.map((gif) => gif.images.original.url);
+      return gifUrls;
+    });
+}
+
+searchBtn.addEventListener("click", function () {
+  const query = searchInput.value;
+  searchGifs(query)
+    .then((gifUrls) => {
+      // Select a GIF from the results
+      const gifUrl = gifUrls[Math.floor(Math.random() * gifUrls.length)];
+      // Send a message with the GIF to the chat
+      const gifImg = document.createElement("img");
+      gifImg.src = gifUrl;
+      chatt.append(gifImg);
+      socket.emit("chat_message", room, message);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
 
 const inRoom = () => {
   if (storedUsername) {
-    h1.innerText=`Chattrum: ${storedRoomName}`;
+    h1.innerText = `Chattrum: ${storedRoomName}`;
     displayMessage(`Välkommen till ${storedRoomName} ${storedUsername}`);
     socket.emit("user_connected_to_room", storedUsername);
   }
@@ -37,7 +70,7 @@ const inRoom = () => {
     displayMessage(`${username}: ${message}`);
   });
 
-  // Lyssna på chattknappen 
+  // Lyssna på chattknappen
   chattBtn.addEventListener("click", sendMessage);
 
   const leaveRoom = (room, username) => {
@@ -84,14 +117,14 @@ const inRoom = () => {
   });
 };
 
-// chatt 
+// chatt
 const displayMessage = (message) => {
   const li = document.createElement("li");
   li.innerText = message;
   chatt.appendChild(li);
 };
 
-// Anslut till rummet med det lagrade användarnamnet 
+// Anslut till rummet med det lagrade användarnamnet
 socket.on("connect", () => {
   if (storedUsername) {
     socket.emit("user_connected_to_server", storedUsername);
